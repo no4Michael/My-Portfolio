34773d39d25652e225b2fdc43be3a1e02013c40c
no4Michael <169057679+no4Michael@users.noreply.github.com>
2024-05-12 16:40:03 +0500

    Add files via upload

---
+++ Calculator1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+// This is Just Calculator On C++.
+
+int main()
+{
+    float num1;
+    cout << "Enter your num1: ";
+    cin >> num1;
+
+    float num2;
+    cout << "Enter your num2: ";
+    cin >> num2;
+
+    char sym;
+    cout << "Enter Symbol +, -, *, /: ";
+    cin >> sym;
+
+    if (sym == '+') {
+        cout << "Result: " << num1 + num2 << endl;
+    }
+    else if (sym == '-') {
+        cout << "Result: " << num1 - num2 << endl;
+    }
+    else if (sym == '*') {
+        cout << "Result: " << num1 * num2 << endl;
+    }
+    else if (sym == '/') {
+        cout << "Result: " << num1 / num2 << endl;
+    }
+}
